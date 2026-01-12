753943a79f89a380cd3cfad6227b5dae9e91fc14
sarinitc <cheavsaren65@gmail.com>
2026-01-12 16:32:38 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/sarinitc/Demojava.git git push -u origin main

---
+++ src/DemoVariable.java
@@ -0,0 +1,29 @@
+public class DemoVariable {
+    public static void main(String[] args){
+
+      String name= "CheavSarin";
+      int age = 20;
+      double height= 1.7;
+      char gender ='f' ;
+      boolean isStudent= false;
+      System.out.println("Name:"+ name);
+      System.out.println("Age:"+ age);
+      System.out.println("Height:"+ height );
+      System.out.println("Gender:"+ gender );
+      System.out.println("isStudent:"+ isStudent);
+        System.out.println("isStudent:"+ isStudent);
+        System.out.println("isStudent:"+ isStudent);
+        System.out.println("isStudent:"+ isStudent);
+        System.out.println("isStudent:"+ isStudent);
+        System.out.println("isStudent:"+ isStudent);
+
+
+
+
+
+
+
+    }
+}
+
+
