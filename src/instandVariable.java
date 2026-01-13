fe3e7496e5c465a59a5d1a854eecb4d326272222
sarinitc <cheavsaren65@gmail.com>
2026-01-13 16:37:40 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/sarinitc/Demojava.git git push -u origin main

---
+++ src/instandVariable.java
@@ -0,0 +1,16 @@
+public class instandVariable {
+    public static void main (String [] args){
+        String name= "Sarin";
+        int age = 20;
+        double score= 444;
+        boolean isStudent = true;
+        System.out.println("Name:" + name);
+        System.out.println("score:" + score);
+        System.out.println("isStudent:" + isStudent);
+
+
+    }
+
+
+
+}
