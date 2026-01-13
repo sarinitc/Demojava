64cc87072d6118ae7de9258390a15ad068d99341
sarinitc <cheavsaren65@gmail.com>
2026-01-13 16:37:40 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/sarinitc/Classes.git git push -u origin main

---
+++ src/staticVariabl.java
@@ -0,0 +1,14 @@
+public class staticVariabl {
+    static String schoolName= "SETEC";
+    static int age = 20;
+    static double score = 22.2;
+    static boolean isStudent= true;
+    public static void main(String [] args){
+        System.out.println(schoolName);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(score);
+        System.out.println(true);
+    }
+
+}
